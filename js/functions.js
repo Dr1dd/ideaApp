6518d5b12f0bb487ba0aae11344031a8afa7b713
Laurynas Leišis <lauris.leisis@gmail.com>
2019-12-15 01:18:26 +0200

    almost done

--- js/functions.js
+++ js/functions.js
@@ -9,9 +9,10 @@ $(function() {
 	$("#addTab").on("click", function(){
 		window.location= "addIdea.html";
 	});
-	$("#addidea").on("click", function(){
-		var text = document.getElementById('1').innerHTML;
+	$("#addbutton").on("click", function(){
+		var text = document.getElementById('ideaContent').innerHTML;
 		front.send('save-data',app.getPath('userData'), text)
+		window.location= "main.html";
 	});
 	$("#search").on("click", function(){
 		if($("#searchInput").css("visibility") == "hidden") {
@@ -31,6 +32,18 @@ $(function() {
 		setTimeout(function(){
 			location.reload();
 		}, 500);
+	});
+	$("#tags").keypress(function (e) {
+	  if (e.key === ' ' || e.key === 'Spacebar') {
+	  	var div = document.createElement("div");
+	  	div.classList.add("tag");
+	  	div.setAttribute("onclick", "deleteTag(this)");
+	  	div.innerHTML = document.getElementById("tags").value;
+	  	document.getElementById("tags").value = "";
+	  	var container = document.getElementById("tagBlock");
+	  	$(div).insertBefore('input.tagInput');
+	    e.preventDefault()
+	  }
 	});
 });
 function openCloseDrawer(){
@@ -58,9 +71,23 @@ window.onload = function(){
 	front.send('get-data', app.getPath('userData'));
 }
 front.on('get-data-result', function(msg){
-		document.getElementById('help').innerHTML = msg;
+	var div = document.createElement("div");
+	div.classList.add("ideasList");
+	var divtimestamp = document.createElement("div");
+	divtimestamp.classList.add("ideaTime");
+	var divcontent =document.createElement("div");
+	divcontent.classList.add("ideaContent");
+	divcontent.innerHTML = msg;
+	div.appendChild(divtimestamp);
+	div.appendChild(divcontent);
+	document.getElementById('allIdeas').appendChild(div);
 })
-function addIdea(){
-	var text = document.getElementById('1').innerHTML;
-	front.send('save-data',app.getPath('userData'), text)
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+}
+function deleteTag(div){
+	$(function() {
+		$(div).remove();
+	});
 }
--- main.js
+++ main.js
@@ -2,17 +2,38 @@ const back = require('androidjs').back;
 const fs = require('fs');
 const path = require('path');
 
-
 back.on('save-data', function(filepath, msg){
-	fs.writeFile(path.join(filepath, 'data.txt'), msg, function(err){
+	fs.writeFile(path.join(filepath, Date.now()+'.txt'), msg, function(err){
 		if(err) throw err;
 		console.log('file saved')
-	})
-})
+	});
+	fs.readdir(filepath, function (err, files) {
+    //handling error
+	    if (err) {
+	        return console.log('Unable to scan directory: ' + err);
+	    } 
+	    //listing all files using forEach
+	    files.forEach(function (file) {
+	        // Do whatever you want to do with the file
+	        console.log(file); 
+	    });
+	});
+});
 
 back.on('get-data', function(filepath){
-	fs.readFile(path.join(filepath, 'data.txt'), 'utf-8', function(err, data){
-		if(err) back.send('get-data-result', 'error');
-		else back.send('get-data-result', data);
-	})
-})
+	fs.readdir(filepath, function (err, files) {
+    //handling error
+	    if (err) {
+	        return console.log('Unable to scan directory: ' + err);
+	    } 
+	    //listing all files using forEach
+	    files.forEach(function (file) {
+	        
+			fs.readFile(path.join(filepath, file), 'utf-8', function(err, data){
+				if(err) back.send('get-data-result', 'error');
+				else back.send('get-data-result', data);
+			})
+	        console.log(file); 
+	    });
+	});
+});
